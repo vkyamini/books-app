2245e81e4375427492f7f0aca73ddd016b96fc4d
Yamini VK <yamini@Ashishs-MacBook-Pro.local>
2023-06-09 22:39:52 -0700

    add QUERY_USER

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_USER = gql`
+  query user {
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks
+    }
+  }
+`;
